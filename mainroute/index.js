ca95d0ac9835fad271040ec3a97f031ba87cb556
unknown <arefinemon308@gmail.com>
2026-02-17 16:32:04 +0600

    mainroute updated

--- mainroute/index.js
+++ mainroute/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 
 import userRoute from "../route/user.route.js";
+import authRoute from "../route/auth.route.js";
+import adminRoutes from "../route/admin.route.js";
 
 const router = express.Router();
 
 router.use("/user", userRoute);
+router.use("/auth", authRoute);
+router.use("/admin",adminRoutes);
 
 export default router;
