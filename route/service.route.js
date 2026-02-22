a0c3d4c691cc9c343958229d2f634884df21b85c
unknown <arefinemon308@gmail.com>
2026-02-22 08:51:06 +0600

    service route added

---
+++ route/service.route.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  createService,
+  getAllServices,
+  getMyServices,
+  getSingleService,
+  updateService,
+  deleteService,
+} from "../controller/service.controller.js";
+
+import {
+  protect, isProvider
+} from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", getAllServices);
+router.get("/:id", getSingleService);
+router.post("/", protect, isProvider, createService);
+router.get("/provider/me", protect, isProvider, getMyServices);
+router.patch("/:id", protect, isProvider, updateService);
+router.delete("/:id", protect, isProvider, deleteService);
+
+export default router;
